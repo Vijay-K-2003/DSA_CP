aaade24879f984bfd89957f43cbce160140bcbe3
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2022-08-14 10:26:49 +0530

    268. Missing Number

---
+++ Blind75/268. Missing Number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        vector<int> count(nums.size() + 1, 0);
+        for(int i = 0 ; i < nums.size(); i++) {
+            count[nums[i]] ++;
+        }
+        for(int i = 0 ; i < count.size() ; i++) {
+            if(count[i] == 0) return i;
+        }
+        return -1;
+    }
+};
