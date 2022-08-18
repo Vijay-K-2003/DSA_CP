17c7b67b84f1766b6b44ebefb97455feec418a2a
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2022-08-18 16:08:44 +0530

    2125. Number of Laser Beams in a Bank

--- 2125. Number of Laser Beams in a Bank.cpp
+++ 2125. Number of Laser Beams in a Bank.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        if(bank.size() == 1) return 0;
+        int b_size = bank.size();
+        if(b_size == 1) return 0;
         vector<int> count;
-        for(int i = 0 ; i < bank.size() ; i++) {
+        for(int i = 0 ; i < b_size ; i++) {
             int cnt = 0;
             for(int j = 0 ; j < bank[i].size() ; j++) {
                 if(bank[i][j] == '1') cnt++;
             }
             if(cnt != 0) count.push_back(cnt);
         }
-        if(count.size() == 1 or count.size() == 0) return false;
+        int c_size = count.size();
+        if(c_size == 1 or c_size == 0) return false;
         int prod = 0;
-        for(int i = 0 ; i < count.size()-1 ; i++) {
+        for(int i = 0 ; i < c_size-1 ; i++) {
             prod += count[i] * count[i+1];
         }
         return prod;
