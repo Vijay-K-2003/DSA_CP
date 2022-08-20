62c6ed3976265ea2e866a1fc88a9fe003c73f128
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2022-08-20 18:11:46 +0530

    7. Reverse Integer

---
+++ 7. Reverse Integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int sol = 0;
+        while (x) {
+            int dig = x % 10;
+            x = x / 10;
+            if (sol > (INT_MAX/10) or (sol == INT_MAX/10 and dig >= INT_MAX % 10))
+                return 0;
+            if (sol < (INT_MIN/10) or (sol == INT_MIN/10 and dig <= INT_MIN % 10))
+                return 0;
+            sol = sol*10 + dig;
+        }
+        return sol;
+    }
+};
