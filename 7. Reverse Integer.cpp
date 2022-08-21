b31fefaec7bdf5dec608d1afad5f553ba2ad3d80
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2022-08-21 21:13:01 +0530

    7. Reverse Integer : 2

--- 7. Reverse Integer.cpp
+++ 7. Reverse Integer.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
     int reverse(int x) {
-        int sol = 0;
+        // int sol = 0;
+        // while (x) {
+        //     int dig = x % 10;
+        //     x = x / 10;
+        //     if (sol > (INT_MAX/10) or (sol == INT_MAX/10 and dig >= INT_MAX % 10))
+        //         return 0;
+        //     if (sol < (INT_MIN/10) or (sol == INT_MIN/10 and dig <= INT_MIN % 10))
+        //         return 0;
+        //     sol = sol*10 + dig;
+        // }
+        // return sol;
+        long sol = 0;
         while (x) {
-            int dig = x % 10;
-            x = x / 10;
-            if (sol > (INT_MAX/10) or (sol == INT_MAX/10 and dig >= INT_MAX % 10))
-                return 0;
-            if (sol < (INT_MIN/10) or (sol == INT_MIN/10 and dig <= INT_MIN % 10))
-                return 0;
-            sol = sol*10 + dig;
+            sol = sol*10 + x%10;
+            if (sol > INT_MAX || sol < INT_MIN) return 0;
+            x = x/10;
         }
-        return sol;
+        return (int)sol;
     }
 };
