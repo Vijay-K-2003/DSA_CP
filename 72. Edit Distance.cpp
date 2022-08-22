d268261d18775b02038b44931a8ac9022b5bca5a
Vijay Karanjkar <vijaykaranjkar2003@gmail.com>
2022-08-22 23:43:23 +0530

    72. Edit Distance : 2

--- 72. Edit Distance.cpp
+++ 72. Edit Distance.cpp
@@ -58,25 +58,51 @@ public:
         
         // Dynamic Programming : 
         // 31ms 9.1MB
+//         int m = word1.size();
+//         int n = word2.size();
+//         vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
+//         for (int i = 0 ; i <= m ; i++) dp[i][0] = i;
+//         for (int i = 0 ; i <= n ; i++) dp[0][i] = i;
+//         for (int i = 1 ; i <= m ; i++) {
+//             for (int j = 1; j <= n ; j++) {
+//                 if (word1.at(i-1) == word2.at(j-1))
+//                     dp[i][j] = dp[i-1][j-1];
+
+//                 else 
+//                     dp[i][j] = 1 + min(
+//                         {
+//                             dp[i][j-1],
+//                             dp[i-1][j], 
+//                             dp[i-1][j-1]
+//                         });
+//             }
+//         } 
+//         return dp[m][n];
+        
+        // Optimised
+        // 13 ms 6.5MB
         int m = word1.size();
         int n = word2.size();
-        vector<vector<int>> dp(m+1, vector<int> (n+1, -1));
-        for (int i = 0 ; i <= m ; i++) dp[i][0] = i;
-        for (int i = 0 ; i <= n ; i++) dp[0][i] = i;
+        vector<int> prev(n+1, 0), cur(n+1, 0);
+        // for (int i = 0 ; i <= m ; i++) dp[i][0] = i;
+        for (int i = 0 ; i <= n ; i++) prev[i] = i;
         for (int i = 1 ; i <= m ; i++) {
+            cur[0]= i;
             for (int j = 1; j <= n ; j++) {
                 if (word1.at(i-1) == word2.at(j-1))
-                    dp[i][j] = dp[i-1][j-1];
+                    cur[j] = prev[j-1];
 
                 else 
-                    dp[i][j] = 1 + min(
+                    cur[j] = 1 + min(
                         {
-                            dp[i][j-1],
-                            dp[i-1][j], 
-                            dp[i-1][j-1]
+                            cur[j-1],
+                            prev[j], 
+                            prev[j-1]
                         });
             }
+            prev = cur;
         } 
-        return dp[m][n];
+        return prev[n];
+        
     }
 };
